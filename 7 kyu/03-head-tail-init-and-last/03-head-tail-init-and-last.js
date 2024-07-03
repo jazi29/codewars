d1e250f042e75d6a5796c0bc453440249a4a39c6
Zhazira Satbaeva <79010812+jazi29@users.noreply.github.com>
2024-07-03 17:17:09 +0500

    Create 03-head-tail-init-and-last.js

---
+++ 7 kyu/03-head-tail-init-and-last/03-head-tail-init-and-last.js
@@ -0,0 +1,9 @@
+// TODO: implement the four functions specified.
+
+function head(a) {return a[0]}
+
+function last(a) {return a[a.length - 1]}
+
+function init(a) {return a.slice(0, -1)}
+
+function tail(a) {return a.slice(1)}
